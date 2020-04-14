7a655669278f74b16a24272236075034bb7ae2af
dragonfly161518 <por.nattapat.01@gmail.com>
2020-04-14 18:43:11 +0700

    Add Ekkamai font and use as global

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ampor`,
+    description: `This is my personal site.`,
+    author: `@ampor`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
