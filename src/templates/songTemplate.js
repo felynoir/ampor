0de7e262320e46954450e1e5794251307e9f32e6
dragonfly161518 <por.nattapat.01@gmail.com>
2020-05-13 15:51:20 +0700

    login spotify and redirection

--- src/templates/songTemplate.js
+++ src/templates/songTemplate.js
@@ -4,23 +4,89 @@ import Layout from '../components/Song/Layout'
 import TypingLetter from '../components/TypingLetter'
 
 import Spotify from '../components/Spotify'
-import { Text } from 'rebass'
 import { useAuth } from '../components/Spotify/authContext'
 
-const SongTemplate = ({
-  data, // this prop will be injected by the GraphQL query below.
-}) => {
-  const { isAuthenticated } = useAuth()
+import styled from 'styled-components'
+import { Text, Flex, Box } from 'rebass'
+import Axios from 'axios'
 
-  console.log(data)
+const Container = styled(Flex)`
+  flex-direction: column;
+  padding: 0 40px;
+`
+
+const MediaContainer = styled(Flex)`
+  height: 100vh;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`
+const SongCover = styled.img`
+  width: 100%;
+`
+
+const SpotifyLogin = styled.button`
+  background: #1db954;
+  color: white;
+  border-radius: 500px;
+  padding: 11px 32px 9px;
+  padding-top: 11px;
+  padding-right: 32px;
+  padding-bottom: 9px;
+  padding-left: 32px;
+  display: inline-block;
+  -ms-touch-action: manipulation;
+  touch-action: manipulation;
+  cursor: pointer;
+  background-image: none;
+  border: 1px solid transparent;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`
+
+const SongTemplate = ({ data: { song } }) => {
+  const { isAuthenticated } = useAuth()
+  const { full_title, song_art_image_url } = song
+  const handleLoggin = async () => {
+    if (typeof window === undefined) return
+    const res = await Axios.get('http://localhost:8888/login', {
+      withCredentials: true,
+    })
+    console.log(document.cookie)
+    if (res.status === 200 && !!res.data.location)
+      window.location.href = res.data.location
+  }
+
   return (
     <Layout>
-      <Text mb={3} color="primary" fontSize={[2, 3, 4]} textAlign="center">
-        TODAY SONG
-        <TypingLetter>{'No Title'}</TypingLetter>
-      </Text>
-      {isAuthenticated ? <Spotify /> : <div>login pls</div>}
-      {/* <Vibe style={{ width: '100%' }} /> */}
+      <Container>
+        <MediaContainer>
+          <Box>
+            <Text
+              mb={3}
+              color="primary"
+              fontSize={[2, 3, 4]}
+              textAlign="center"
+            >
+              {full_title}
+            </Text>
+            {isAuthenticated ? (
+              <Spotify />
+            ) : (
+              <SpotifyLogin onClick={() => handleLoggin()}>
+                Spotify Login
+              </SpotifyLogin>
+            )}
+            {/* <Vibe style={{ width: '100%' }} /> */}
+          </Box>
+          <Box>
+            <SongCover src={song_art_image_url} alt="song cover" />
+          </Box>
+        </MediaContainer>
+        <div>Lyrics is here</div>
+      </Container>
     </Layout>
   )
 }
@@ -31,6 +97,8 @@ export const pageQuery = graphql`
   query($geniusId: String!) {
     song: songFromGenius(id: { eq: $geniusId }) {
       id
+      full_title
+      song_art_image_url
     }
   }
 `
